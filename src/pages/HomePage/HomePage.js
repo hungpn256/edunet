6b385491468968cc20661bcde6e242285d01541f
Hung Pham <phamnanghung.25@gmail.com>
2021-05-19 19:07:04 +0700

    fix warning css

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Trans, useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 function HomePage() {
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
   const changeLanguage = (language) => {
     i18n.changeLanguage(language);
   };
   return (
     <div>
       <h2>đây là tiếng việt</h2>
-      <Trans i18nKey='hello'>abc</Trans>
-      <button onClick={() => changeLanguage('en')}>aaa</button>
-      <button onClick={() => changeLanguage('vn')}>aaa</button>
+      {t('hello')}
+      <button onClick={() => changeLanguage('en')}>en</button>
+      <button onClick={() => changeLanguage('vn')}>vn</button>
     </div>
   );
 }
